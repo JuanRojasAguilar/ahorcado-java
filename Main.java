8ca2e009d335028c0f0a22b5297c614b074ec3c3
Juan Rojas <yo217@hotmail.es>
2024-06-29 13:42:30 -0500

    :construction: word complete

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import java.text.MessageFormat;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
   protected static Scanner userInput = new Scanner(System.in);
   protected static byte seleccion;
+
   public static void main(String[] args) {
     Juego.iniciar();
     seleccion = userInput.nextByte();
@@ -18,25 +20,27 @@ public class Main {
       default:
         Juego.salir();
         break;
-        
+
     }
   }
+
   static class Juego {
     private static String[] opciones = { "Juego Nuevo", "Salir" };
     private static String[] palabras = {
-      "filtrado",
-      "compilar",
-      "formatear",
-      "machete",
-      "jholver",
-      "javascript",
-      "backend",
-      "fullstack",
-      "instancia",
-      "referencia"
+        "filtrado",
+        "compilar",
+        "formatear",
+        "machete",
+        "jholver",
+        "javascript",
+        "backend",
+        "fullstack",
+        "instancia",
+        "referencia"
     };
-    private static Random luckyNum = new Random();
-  
+    private static int luckyNum = (int) new Random().nextInt(10);
+    private static boolean isDone = false;
+
     static void iniciar() {
       System.out.println("Bienvenidos al ahorcado de Jholver!!");
       System.out.println("Por favor elige la opcion");
@@ -47,28 +51,47 @@ public class Main {
     }
 
     static void jugar() {
-      boolean isDone = false;
-      String intento;
-      int numeroPalabra = luckyNum.nextInt(10);
-      while (isDone == false) {
-        String luckyWord = palabras[numeroPalabra];
-        char[] adivinadas = [];
+      while (!isDone) {
+        String luckyWord = palabras[luckyNum];
 
-        System.out.println("Adivina de una letra o la palabra!");
-        imprimirPantalla(null,luckyWord);
+        imprimirPantalla(null, luckyWord);
 
       }
     }
 
-    private static void imprimirPantalla(String guess, String palabraMagica) {
-        limpiarPantalla();
-      System.out.print(">>");
-      intento = userInput.nextLine();
-      for(int i = 0; i < palabraMagica.length(); i++) {
-        for (int j =)
+    private static void imprimirPantalla(ArrayList<String> guess, String palabraMagica) {
+      limpiarPantalla();
+      ArrayList<String> adivinadas = new ArrayList<String>();
+      String intento;
+      System.out.println("Adivina de una letra o la palabra!");
+      for (int i = 0; i < palabraMagica.length(); i++) {
+        for (int j = 0; j < adivinadas.size(); j++) {
+          if (palabraMagica.charAt(i) == adivinadas.get(j).charAt(0)) {
+            System.out.print(palabraMagica.charAt(i));
+          }
+        }
         System.out.print('_');
       }
       System.out.println();
+      System.out.print(">>");
+      intento = userInput.nextLine();
+
+      if (intento.length() > 1) {
+        if (intento == palabraMagica) {
+          System.out.println("Lo lograste!!!");
+          esperar();
+          isDone = true;
+        }
+      } else {
+        if (palabraMagica.contains(intento)) {
+          System.out.println(palabraMagica.indexOf(intento));
+          adivinadas.add(intento);
+          esperar();
+        }
+      }
+
+      imprimirPantalla(adivinadas, palabraMagica);
+
     }
 
     static void limpiarPantalla() {
@@ -84,6 +107,13 @@ public class Main {
       }
     }
 
-    static void salir() { System.exit(0); }
+    static void esperar() {
+      System.out.println("Presiona enter para continuar...");
+      userInput.nextLine();
+    }
+
+    static void salir() {
+      System.exit(0);
+    }
   }
 }
